034fca3cc8a71e15f2885a52908a4fae926c2d86
akhilesh-k-y <mrakhi432@gmail.com>
2020-09-05 21:50:56 +0530

    initial commit

---
+++ src/elements/footer.js
@@ -0,0 +1,4 @@
+import React from 'react'
+
+const Base = ()=> <h1 >Helo world!!!!!!</h1>
+export default Base;
---
+++ src/elements/login.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const lgin = () => {
+    return (
+        <div className="container lg1">
+            <form>
+                <div className="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div className="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1"></input>
+                </div>
+                <div className="form-group form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default lgin
